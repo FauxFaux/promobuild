4f77e8ed446016b2fe3529a3b301a07c98493e64
Chris West (Faux) <git@goeswhere.com>
2010-10-03 23:29:18 +0100

    Reinstate url rewriting, and layouts

--- src/main/java/com/goeswhere/promobuild/ui/HomeController.java
+++ src/main/java/com/goeswhere/promobuild/ui/HomeController.java
@@ -1,17 +1,16 @@
-package com.goeswhere.promobuild;
+package com.goeswhere.promobuild.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public ModelAndView home() {
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("message", "Hello World!");
-        return mav;
+    public void home(Model m) {
+    	m.addAttribute("title", "Welcome to pony");
+    	m.addAttribute("message", "Hello World!");
     }
 
     @RequestMapping("/")
